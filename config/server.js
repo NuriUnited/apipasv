a2dde7e761de761e4b877a1969d27a4b62135fd6
Nurbol  Yeshman <nurbol_@mail.ru>
2021-09-17 10:11:56 -0700

    add users tests

--- config/server.js
+++ config/server.js
@@ -1,3 +1,9 @@
 import { config } from 'dotenv';
+import AuthHelper from '../helpers/auth.helpers';
 
 config();
+before(async function() {
+    const authHelper = new AuthHelper();
+    await authHelper.get(process.env.LOGIN, process.env.PASSWORD);
+    process.env.TOKEN = authHelper.response.body.token;
+});
